133a4212a0188d726f4a5269c1cc9c442b15abac
Your Name <your-github-email@example.com>
2026-05-30 22:13:48 -0400

    Initial Commit

---
+++ anotherExample/anotherExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  anotherExampleApp.swift
+//  anotherExample
+//
+//  Created by Anvi Dobhal on 5/30/26.
+//
+
+import SwiftUI
+
+@main
+struct anotherExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
